44a67b64a83e79a72499224d4e301e28c14d34cf
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-22 14:25:33 +0630

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,11 @@ from telegram import Bot
 
 server = Flask(__name__)
 
+BotToken = "5943587962:AAHKiQ2-_TDtDReJd1ac0vZ4413Tpvpr-jU"
+
 @server.route("/")
 def hello():
+    response = (Bot(BotToken).send_message(5030058973, 'hello'))
     return "Hello World!"
 
 if __name__ == "__main__":
