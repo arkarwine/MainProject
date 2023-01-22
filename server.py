b1f1c6486de61af4d10aa424cc360287cf9fb183
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-22 14:46:08 +0630

    Update server.py

--- server.py
+++ server.py
@@ -11,5 +11,15 @@ def hello():
     response = (Bot(BotToken).send_message(5030058973, 'hello'))
     return "Hello World!"
 
+@server.route("/webhook", methods=['POST'])
+def Webhook_handler():
+    data = request.stream.read().decode('utf-8')
+    bot = Bot(BotToken)
+    dp = Dispatcher(bot, None, workers=0, use_context=True)
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    update = Update.de_json(json.loads(data), bot)
+    dp.process_update(update)
+    return 'ok', 200
+
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)
