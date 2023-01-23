fc4a358feab49eb6fc3720d80592bf4211a7c9ef
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-23 17:55:06 +0630

    Update server.py

--- server.py
+++ server.py
@@ -10,5 +10,20 @@ server = Flask(__name__)
 def Home():
     return "Hello World !"
 
+Bot = Client("userbot", "22444092", "bc6c9d84db95809f59bb96af90ccffd3")
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+
+@Bot.on_message(filters.text)
+async def echo(bot: Client, update: Message):
+    await update.reply(
+        update.text
+    )
+
 
 threading.Thread(target=lambda: server.run(host='0.0.0.0', port=1337)).start()
+
+
+Bot.run()
