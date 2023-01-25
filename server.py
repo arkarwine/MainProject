42489c93754ae9ea420a6099bdaf2047dff8f538
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-25 19:11:23 +0630

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import json
+import threading
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, ContextTypes, MessageHandler, Filters
+from telegram.ext import Application, ContextTypes, MessageHandler, filters
 
 
 server = Flask(__name__)
@@ -10,28 +11,25 @@ server = Flask(__name__)
 BotToken = "5943587962:AAHKiQ2-_TDtDReJd1ac0vZ4413Tpvpr-jU"
 
 
-def echo(update: Update, context: ContextTypes):
-    reaponse = update.effective_message.reply_text(
-        update.effective_message.text+ ", from Mogenius."
+async def echo(update: Update, context: ContextTypes):
+    reaponse = await update.effective_message.reply_text(
+        update.effective_message.text
     )
 
 
 @server.route("/")
 def hello():
-    response = (Bot(BotToken).send_message(5030058973, 'Hi !'))
     return "Hello World !"
 
 
-@server.route("/webhook", methods=['POST'])
-def Webhook_handler():
-    data = request.stream.read().decode('utf-8')
-    bot = Bot(BotToken)
-    dp = Dispatcher(bot, None, workers=0, use_context=True)
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    update = Update.de_json(json.loads(data), bot)
-    dp.process_update(update)
-    return 'ok', 200
+def main():
+    APP = Application.builder().token(BotToken).build()
+    
+    APP.add_handler(MessageHandler(filters.TEXT, echo))
+
+    APP.run_polling()
 
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0', port=1337)
+    threading.Thread(target=lambda: server.run(host='0.0.0.0', port=1337)).start()
+    main()
