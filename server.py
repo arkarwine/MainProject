477ebb0c050134d8b4e04a87e257e3436b559300
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-26 16:09:02 +0630

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,30 @@
-from telethon.sync import TelegramClient, events
-from telethon.sessions import StringSession
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from flask import Flask
-import logging
 import threading
+import logging
+
+
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+    return "Hello World !"
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
 
-server = Flask(__name__)
-
-@server.route('/')
-def Home():
-    return "Hello from Mogenius !!"
+Bot = Client(":memory:", "22444092", "bc6c9d84db95809f59bb96af90ccffd3", session_string="BQFWeDwAEH5b9-WgXfpyL_guf3NtdzMTHHcxulXPmftScIBodJ6kWbl8yXKb136juutCKoKXoImH2y9Al1HD5UhENnsdwntdwJ8DzXm_NLsp-XLAdr4TqmI13Rcs9wFHJp8C4Yvvo2LgHrZPt2nLPF5dHvYqAh0vTIyct0lZhmsmGs5FrYKQGvcVpF0v842td_oWLB9cobTl1uEcS2M3_SGitQcCcrCnVbeSQ9rvGX28TcuuH5DJfxclL1Er_GqiRaUMKQdXXG7GUNguTFOCkHFYCNjDB_jCZ1ONdXAAJdHQ62vvyad0WeuI93i_8LpfV8Q4jBgFz9nXq28gNqvbxVOYVshhKgAAAAFHQ0hsAA", in_memory=True)
 
 
-with TelegramClient(StringSession("1BVtsOH0Bu2OCV08TBRHRvXKONboqEtvdibAbkRZySFIwcKjgDVXhdvPOE5TwmAFTGXcnR3iMhVTI6Ru85qQkJG2xaXjX7PYZQj-slUB9k_Juv3SddRR57c42_wqxnhj6ugatrNZnRs4xfyAydFv5PDFKNiG-75oGnIBWwVypVLP0X7f8hUK9_lCYCP4IwGfX6fWugb__3hsuwVQ5xuaoV47dVBqsEB8E4_eVtWp44Y16CnDZG0Pob2HCLZ9l3DT9cyA8whtfOQiT_W3rqALU0-d3XDKn7Sdg7jV79zxwBPqPCDvMX03NYqqrFTyCAUY3FWGbrN4IBU1RZr85qO4ZM1JSgYWI-Mk="), "22444092", "bc6c9d84db95809f59bb96af90ccffd3") as client:
-    
-    client.send_message('me', 'me')
+@Bot.on_message(filters.private)
+async def echo(_, update: Message):
+    await update.reply(
+        update.text
+    )
 
-    @client.on(events.NewMessage)
-    async def my_event_handler(event):
-        await event.reply(event.raw_text)
-    
-
-    if __name__ == "__main__":
-        threading.Thread(target=lambda: server.run(host='0.0.0.0', port=1337)).start()
-        client.start()
-        client.run_until_disconnected()
+if __name__ == "__main__":
+    threading.Thread(target=lambda: server.run(host='0.0.0.0', port=1337)).start()
+    Bot.run()
