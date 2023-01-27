2558edc841b475799c9bf21f2c2956f5cfd80e4a
arkarwine <112911561+arkarwine@users.noreply.github.com>
2023-01-27 16:43:06 +0630

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ from pyrogram.types import Message
 from pytgcalls import PyTgCalls, idle
 from pytgcalls.types import AudioPiped
 from flask import Flask
-import os
-import re
+import requests
 import threading
+import re
 import logging
 import asyncio
 import threading
+import json
 
 
 server = Flask(__name__)
@@ -22,29 +23,36 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 
 
-Bot = Client(":memory:", "22444092", "bc6c9d84db95809f59bb96af90ccffd3", session_string="BQFWeDwAEH5b9-WgXfpyL_guf3NtdzMTHHcxulXPmftScIBodJ6kWbl8yXKb136juutCKoKXoImH2y9Al1HD5UhENnsdwntdwJ8DzXm_NLsp-XLAdr4TqmI13Rcs9wFHJp8C4Yvvo2LgHrZPt2nLPF5dHvYqAh0vTIyct0lZhmsmGs5FrYKQGvcVpF0v842td_oWLB9cobTl1uEcS2M3_SGitQcCcrCnVbeSQ9rvGX28TcuuH5DJfxclL1Er_GqiRaUMKQdXXG7GUNguTFOCkHFYCNjDB_jCZ1ONdXAAJdHQ62vvyad0WeuI93i_8LpfV8Q4jBgFz9nXq28gNqvbxVOYVshhKgAAAAFHQ0hsAA", in_memory=True)
+Bot = Client(":memory:", "22444092", "bc6c9d84db95809f59bb96af90ccffd3", session_string="BQFWeDwAij1DFjK9HSSJrJnBfh6D88rGZtZIFk3H_3yt9S1QdwiUNiAQoujVh9F1Ktf5urb4g0wTwidSifYAImQ9HGSTmucOPlu59s4X0g5RJBV736bTGHpe_HWK5gvTRYelzgBm-rDzpxiTo-Om0FOyYfC-8MZ1SlSBgU0MaSlgVrP7NT5D2kkvaxNc--AIaCvndvk0GJ2pfMX5batga-lGTwoSbrnal1QXjrl1QIBYimWrCJ8NN6chWH9TMdDFcyuYSZAjazeBuVl0b6Dcz6MtqWGLglKToTZZFOGSp_f_cV2Xa3dmhTWkQWEqdJu5EmmchZkbV5SMa3odAhq4LbaAQpo4IwAAAAFHQ0hsAA", in_memory=True)
 app = PyTgCalls(Bot)
 
 
-@Bot.on_message(filters.private)
-async def echo(_, update: Message):
+@Bot.on_message(filters.c)
+async def echo(bot: Client, update: Message):
     try:
-        term = update.text
+        term = re.findall(r'.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*', update.text)[0]
         toEdit = await update.reply(
             'Starting...'
         )
-        if re.findall(r'^(http|https):\/\/[^ "]+$', term):
-            url = os.popen(f'python -m yt_dlp -g -f "bestaudio" "{term}"').read()
-        else:
-            url = os.popen(f'python -m yt_dlp -g -f "bestaudio" ytsearch1:"{term}"').read()
-        await update.reply(
-            url
-        )
+
+        url = "https://ytstream-download-youtube-videos.p.rapidapi.com/dl"
+
+        querystring = {"id":term}
+
+        headers = {
+            "X-RapidAPI-Key": "3d28911b5emshfa3878e04bad055p1d8d4ajsn3656fe6360e3",
+            "X-RapidAPI-Host": "ytstream-download-youtube-videos.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        url = json.loads(response.text)['formats'][-1]['url']
+
         try:
             await app.join_group_call(
                 -1001787879635,
                 AudioPiped(
-                    "https://rr4---sn-hp57yn7y.googlevideo.com/videoplayback?expire=1674760673&ei=gX3SY7CUMIeRigSqwaiwCg&ip=195.181.162.179&id=o-AFIbixrv_WMKeK-kUZxFslJZhXy9s_WXcstJq-wmiN7V&itag=251&source=youtube&requiressl=yes&mh=Ux&mm=31%2C26&mn=sn-hp57yn7y%2Csn-p5qlsn76&ms=au%2Conr&mv=m&mvi=4&pl=23&initcwndbps=640000&spc=H3gIhsCtwGQb55cbYOMsk5gHRIBAwmg&vprv=1&svpuc=1&mime=audio%2Fwebm&gir=yes&clen=3728829&dur=209.581&lmt=1670447586727367&mt=1674738806&fvip=5&keepalive=yes&fexp=24007246&c=ANDROID&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRAIgO5-vYU57mXdAK6GPdDlGtM1ODWBniuCnCclW6P3Z358CIEyVuCYpYcTdgRlVkU25N3Y9kxpiEe1RTaSlc3FNNQyh&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAPXPaYgrFkHFT_mlLocN01yorLhgbe4Tt8K4uCGB8GXxAiBb1pY6FfMnaO2xgWWEdhLYUt5jvWOEVsYp8cCrMXUcHA%3D%3D",
+                    url,
                 )
             )
         except:
