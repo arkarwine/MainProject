155fac545ef91db60cfb4d6b4678039dd0ec1dfd
arkarwine <arkarwine13579@gmail.com>
2023-09-23 14:06:07 +0630

    update all shit

--- src/utils/ytDl.ts
+++ src/utils/ytDl.ts
@@ -1,14 +1,23 @@
 // import { type } from "./types.ts";
 
+import { YoutubeApi } from "./types.d.ts";
+
 export const YoutubeDl = async (yturl: string): Promise<string> => {
-    const format = "ba[ext=m4a]";
+    const api = `https://ytstream-download-youtube-videos.p.rapidapi.com/dl?id=${yturl}`;
+    const options: RequestInit = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "3d28911b5emshfa3878e04bad055p1d8d4ajsn3656fe6360e3",
+            "X-RapidAPI-Host":
+                "ytstream-download-youtube-videos.p.rapidapi.com",
+        },
+    };
+    const response = await fetch(api, options);
+    const result: YoutubeApi = JSON.parse(await response.text());
 
-    const cmd = new Deno.Command("yt-dlp", {
-        args: ["-f", format, "--get-url", yturl],
-    });
-    const { stdout } = await cmd.output();
+    // console.debug(result);
 
-    const result = new TextDecoder().decode(stdout);
-
-    return result;
+    const youtube = result.formats.slice(-1)[0].url;
+    return youtube;
 };
